354c637aac67fb31138dc0c7d84a85acaa83916f
rizkyy12 <rizky.andhika@mhs.polman-bandung.ac.id>
2021-03-16 10:29:14 +0700

    initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/rizky/sena_ws/src
+# Build directory: /home/rizky/sena_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("sena/sena_controller")
---
+++ build/sena/sena_controller/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
+PROJECT_CATKIN_DEPENDS = "roscpp;rospy;std_msgs".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lsena_controller".split(';') if "-lsena_controller" != "" else []
+PROJECT_NAME = "sena_controller"
+PROJECT_SPACE_DIR = "/home/rizky/sena_ws/devel"
+PROJECT_VERSION = "0.0.0"
